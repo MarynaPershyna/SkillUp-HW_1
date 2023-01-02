3017ccb2038bfd755293479231245c06ef90438f
Maryna <mv15121983@gmail.com>
2023-01-02 13:22:16 +0200

    add task 2-1

---
+++ Desktop/JS_HW_2/task_2-1.js
@@ -0,0 +1,26 @@
+// const logItems = function (array) {
+//   // твій код
+// };
+// let logItems = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+// function displayArr(arr){
+//   for (let idx = 0; idx < arr.length; idx++) {
+//     console.log = arr[idx];
+    
+//   }
+// }
+// displayArr(logItems)
+
+
+let fruits = ["apple", "banana", "pear", "melon"];
+let users = ["vova", "sara", "bob", "mike"];
+let numbers = [10, 20, 30, 40, 45, 4];
+
+function dispalyArr(arr) {
+    for (let idx = 0; idx < arr.length; idx++) {
+        console.log(arr[idx]);
+    }
+}
+
+dispalyArr(users);
+dispalyArr(fruits);
+dispalyArr(numbers);
